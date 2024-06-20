e7442015a578703b84c2bf1621baf974e33c7453
VJLOCKED <vishwanathjumanal412@gmail.com>
2024-06-20 08:06:57 +0530

    Modified Code

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -1,18 +1,22 @@
 import { FC } from 'react';
-import ReactTypescriptTemplateLogo from '../../assets/images/react-typescript-template.png';
+import Video from '../Video';
 
 const HomePage: FC = () => {
   return (
-    <div className="text-center">
-      <h1 className="text-4xl font-bold my-2">React Typescript Template</h1>
-      <img
-        src={ReactTypescriptTemplateLogo}
-        width={500}
-        className="mx-auto"
-        alt="React-Typescript-Template"
-      />
-    </div>
-  );
+    // <div className="text-center">
+    //   <h1 className="text-4xl font-bold my-2">React Typescript Template</h1>
+    //   <img
+    //     src={ReactTypescriptTemplateLogo}
+    //     width={500}
+    //     className="mx-auto"
+    //     alt="React-Typescript-Template"
+    //   />
+    //</div>
+
+    <Video/>
+    
+  )
 };
+
 
 export default HomePage;
---
+++ src/components/Video.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Video() {
+  return (
+        <video src="https://sxcontent9668.azureedge.us/cms-assets/assets/Flight_4_Drone_Website_1080_P_8b7a57a59a.mp4" 
+        autoPlay muted ></video>
+  )
+}
